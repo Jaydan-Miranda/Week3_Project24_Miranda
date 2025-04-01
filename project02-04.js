e082a7ce981b7638bea69037f4e1410ea1dba650
Jaydan-Miranda <miry08733@gmail.com>
2025-04-01 09:58:08 -0500

    commited

--- project02-04.js
+++ project02-04.js
@@ -16,11 +16,33 @@
    const SALAD_PRICE = 7.95
    const SALES_TAX = 0.07
 
+function calcTotal(){
 
+   let cost = 0;
+
+   buyChicken = document.getElementById("chicken").checked;
+   buyHalibut = document.getElementById("halibut").checked;
+   buyBurger = document.getElementById("burger").checked;
+   buySalmon = document.getElementById("salmon").checked;
+   buySalad = document.getElementById("salad").checked;
+
+
+   CHICKEN_PRICE += buyChicken ? CHICKEN_PRICE : 0;
+   HALIBUT_PRICE += buyHalibut ? HALIBUT_PRICE : 0;
+   BURGER_PRICE += buyBurger ? BURGER_PRICE : 0;
+   SALMON_PRICE += buySalmon ? SALMON_PRICE : 0;
+   SALAD_PRICE += buySalad ? SALAD_PRICE : 0;
+
+   document.getElementById("foodTotal").addEventListener("click",)
+
+}
 
 
 
 // Function to display a numeric value as a text string in the format $##.## 
  function formatCurrency(value) {
     return "$" + value.toFixed(2);
+    
+
+      
  }
